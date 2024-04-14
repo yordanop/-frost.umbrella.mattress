1cf60d3c2d2663a5fa562c30ce3ca9878e473e06
Eynar <yordanopv@gmail.com>
2024-04-14 14:48:13 -0600

    button changes class

--- resources/js/logic.js
+++ resources/js/logic.js
@@ -1,12 +1,32 @@
 let buttonChng = document.querySelector('#mode-change');
 let lightMode = true;
 
+
 function setToDarkMode(){
     const pTags = document.querySelectorAll('.light');
-    console.log(pTags)
-    for(let pt_i of pTags){
-        pt_i.setAttribute('style', 'color: var(--dark-text);');
+    
+    console.log(lightMode)
+
+    if(lightMode){
+        for(let pt_i of pTags){
+            pt_i.setAttribute('style', 'color: var(--dark-text);');
+            pt_i.classList.remove('light');
+            pt_i.classList.add('dark')
+        }
+        
+        lightMode = false
+        // document.getElementById("MyElement").classList.remove('MyClass');
+    }else{
+        for(let pt_i of pTags){
+            pt_i.setAttribute('style', 'color: var(--light-text);');
+            pt_i.classList.remove('dark');
+            pt_i.classList.add('light')
+        }
+        
+        lightMode = true
+
     }
+    console.log(lightMode)
 }
 
 buttonChng.addEventListener('click', setToDarkMode);
