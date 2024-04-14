1c60fa46a2c64f2667e2739f31f97c14326f3eaa
Eynar <yordanopv@gmail.com>
2024-04-14 15:10:48 -0600

    added light and dark options on css

--- resources/js/logic.js
+++ resources/js/logic.js
@@ -3,22 +3,24 @@ let lightMode = true;
 
 
 function setToDarkMode(){
-    const pTags = document.querySelectorAll('.light');
     
+
     console.log(lightMode)
 
     if(lightMode){
+        const pTags = document.querySelectorAll('.light');
         for(let pt_i of pTags){
-            pt_i.setAttribute('style', 'color: var(--dark-text);');
+            // pt_i.setAttribute('style', 'color: var(--dark-text);');
             pt_i.classList.remove('light');
             pt_i.classList.add('dark')
         }
         
         lightMode = false
-        // document.getElementById("MyElement").classList.remove('MyClass');
+        
     }else{
+        const pTags = document.querySelectorAll('.dark');
         for(let pt_i of pTags){
-            pt_i.setAttribute('style', 'color: var(--light-text);');
+            // pt_i.setAttribute('style', 'color: var(--light-text);');
             pt_i.classList.remove('dark');
             pt_i.classList.add('light')
         }
